75c8f70d35158ee830e2ff8846f6b22f537ea034
qamar002-mdx <qu002@live.mdx.ac.uk>
2024-10-17 16:05:30 +0100

    push project in git

---
+++ js/products.js
@@ -0,0 +1,102 @@
+let products = [
+    {
+        id: 1001,
+        title: 'Math',
+        description: 'The study of numbers, shapes, and patterns, focusing on problem-solving and logical reasoning.',
+        price: '99.99',
+        image: 'images/math.jpg',
+        location: "London",
+        rating: 4,
+        availableInventory: 10
+    },
+    {
+        id: 1002,
+        title: 'English',
+        description: 'Language arts, covering reading, writing, grammar, and comprehension skills.',
+        price: '84.99',
+        image: 'images/english.jpg',
+        location: "Liverpool",
+        rating: 5,
+        availableInventory: 7
+    },
+    {
+        id: 1003,
+        title: 'Science',
+        description: 'Exploration of the natural world through observation, experimentation, and analysis.',
+        price: '79.99',
+        image: 'images/science.jpg',
+        location: "Manchester",
+        rating: 3,
+        availableInventory: 9
+    },
+    {
+        id: 1004,
+        title: 'Music',
+        description: 'The art of combining sounds to create harmony and rhythm, often using instruments.',
+        price: '54',
+        image: 'images/music.jpg',
+        location: "London",
+        rating: 4,
+        availableInventory: 10
+    },
+    {
+        id: 1005,
+        title: 'Artificial Intelligence',
+        description: 'AI is used in applications like robotics, natural language processing, image recognition.',
+        price: '49.99',
+        image: 'images/ai.jpg',
+        location: "Manchester",
+        rating: 5,
+        availableInventory: 8
+    },
+    {
+        id: 1006,
+        title: 'Art',
+        description: 'Creative expression through various mediums like drawing, painting, and sculpture.',
+        price: '63.99',
+        image: 'images/art.jpg',
+        location: "Sheffield",
+        rating: 5,
+        availableInventory: 7
+    },
+    {
+        id: 1007,
+        title: 'Physics',
+        description: 'branch of science that studies matter, energy, and the fundamental forces of nature.',
+        price: '77.99',
+        image: 'images/physics.jpg',
+        location: "Birmingham",
+        rating: 4,
+        availableInventory: 8
+    },
+    {
+        id: 1008,
+        title: 'Computer Science',
+        description: 'The study of computer systems, programming, and problem-solving using technology.',
+        price: '84.99',
+        image: 'images/computer-scie.jpg',
+        location: "London",
+        rating: 2,
+        availableInventory: 9
+    },
+    {
+        id: 1009,
+        title: 'Foreign Language',
+        description: 'Learning a language other than one native tongue to communicate and understand different cultures.',
+        price: '36',
+        image: 'images/language.jpg',
+        location: "Liverpool",
+        rating: 3,
+        availableInventory: 10
+    },
+    {
+        id: 1010,
+        title: 'Business',
+        description: 'The study of past events, societies, and cultures, often involving timelines and critical analysis.',
+        price: '64.99',
+        image: 'images/business.jpg',
+        location: "Birmingham",
+        rating: 5,
+        availableInventory: 8
+    }
+];
